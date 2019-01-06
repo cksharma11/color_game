e4f375541f094c89df0373328268be98bdd795c5
Chandan Kumar <22887527+cksharma11@users.noreply.github.com>
2019-01-06 15:23:48 +0530

    Added end conditions

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const colors = ['red', 'blue', 'green', 'yellow'];
 let positiveScore = 0;
 let negativeScore = 0;
 let colorNameIndex = 1;
+let gameTime = window.prompt('Enter Game Time (Seconds) ');
 
 const changeColorName = function(box) {
   updateScore(box.id, colors[colorNameIndex]);
@@ -18,8 +19,21 @@ const updateScore = function(id, color) {
   if (id == color) {
     positiveScore = positiveScore + 10;
     document.getElementById('score').innerText = positiveScore;
-  } else {
-    negativeScore = negativeScore + 10;
-    document.getElementById('n_score').innerText = negativeScore;
+    return;
   }
+  negativeScore = negativeScore + 10;
+  document.getElementById('n_score').innerText = negativeScore;
 };
+
+let timeCount = 0;
+let playingTime = setInterval(() => {
+  if (timeCount == gameTime) {
+    clearInterval(playingTime);
+    window.confirm(
+      `Game Over\nFinal Score : ${positiveScore -
+        negativeScore}\nWant to play again?`
+    );
+    location.reload();
+  }
+  timeCount++;
+}, 1000);
